454da9ce0b054c4df6b96b594639bba97d2e1464
Koketso-dax <lanchaster.k@gmail.com>
2024-02-13 12:34:54 +0200

    Tested Update

--- console.py
+++ console.py
@@ -139,38 +139,6 @@ class HBNBCommand(cmd.Cmd):
                        if key.startswith(f"{class_name}.")]
             print(len(matches))
 
-    def update_dict(classname, uid, js_dict):
-        """ Util method for attribute handling in update """
-        js = js_dict.replace("'", '"')
-        data_dict = json.loads(js)
-
-        if not classname:
-            print("** class name missing **")
-            return
-        if classname not in storage.class_names():
-            print("** class doesn't exist **")
-            return
-        if uid is None:
-            print("** instance id missing **")
-            return
-
-        key = "{}.{}".format(classname, uid)
-        instance_dict = storage.all()
-
-        if key not in instance_dict:
-            print("** no instance found **")
-            return
-
-        attributes = storage.attributes()[classname]
-        instance = instance_dict[key]
-
-        for attribute, value in data_dict.items():
-            if attribute in attributes:
-                value = attributes[attribute](value)
-            setattr(instance, attribute, value)
-
-        instance.save()
-
     def do_update(self, arg):
         """ Updates one or more fields in an instance """
         if not arg:
@@ -195,20 +163,12 @@ class HBNBCommand(cmd.Cmd):
 
             if key not in instance_dict:
                 print("** no instance found **")
-            elif attribute not in storage.attributes().get(classname, {}):
-                print("** attribute name missing **")
             elif not value:
                 print("** value missing **")
-            elif attribute in ["id", "created_at", "updated_at"]:
-                print("** cannot update id, created_at, and updated_at **")
             else:
-                cast_type = storage.attributes()[classname][attribute]
-                try:
-                    casted_value = cast_type(value)
-                    setattr(instance_dict[key], attribute, casted_value)
-                    instance_dict[key].save()
-                except (ValueError, TypeError):
-                    print("** invvalid value type for attribute **")
+                instance = instance_dict[key]
+                setattr(instance, attribute, value)
+                instance.save()
 
     def postcmd(self, stop, arg):
         """ Print new line after command exercution in non-interactive mode """
